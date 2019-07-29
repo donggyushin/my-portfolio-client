66494f5c6611fcef6a77dfc45492f1024c8c6780
shin donggyu <donggyu9410@gmail.com>
2019-07-29 16:40:03 +0900

    summary of skills

--- src/components/experiences/experiences.js
+++ src/components/experiences/experiences.js
@@ -75,31 +75,52 @@ class Experiences extends React.Component {
             }
           />
           <Card
-            name={"React"}
-            title={"A JavaScript library"}
+            name={"RDB"}
+            title={"Relational database"}
             desc={
-              "React makes it painless to create interactive UIs. Design simple views for each state in your application, and React will efficiently update and render just the right components when your data changes."
+              "A relational database is a set of formally described tables from which data can be accessed or reassembled in many different ways without having to reorganize the database tables. "
             }
           />
           <Card
-            name={"React"}
-            title={"A JavaScript library"}
+            name={"Express"}
+            title={"Web application framework"}
             desc={
-              "React makes it painless to create interactive UIs. Design simple views for each state in your application, and React will efficiently update and render just the right components when your data changes."
+              "Express is a minimal and flexible Node.js web application framework that provides a robust set of features for web and mobile applications."
             }
           />
           <Card
-            name={"React"}
-            title={"A JavaScript library"}
+            name={"Java"}
+            title={"Programing language"}
             desc={
-              "React makes it painless to create interactive UIs. Design simple views for each state in your application, and React will efficiently update and render just the right components when your data changes."
+              "Java is a programming language that produces software for multiple platforms. When a programmer writes a Java application, the compiled code (known as bytecode) runs on most operating systems (OS)"
             }
           />
           <Card
-            name={"React"}
-            title={"A JavaScript library"}
+            name={"Python"}
+            title={"Programing language"}
             desc={
-              "React makes it painless to create interactive UIs. Design simple views for each state in your application, and React will efficiently update and render just the right components when your data changes."
+              "Python is an interpreted, high-level, general-purpose programming language. Python's design philosophy emphasizes code readability with its notable use of significant whitespace. Its language constructs and object-oriented approach aim to help programmers write clear, logical code for small and large-scale projects."
+            }
+          />
+          <Card
+            name={"Swift"}
+            title={"Programing language"}
+            desc={
+              "Swift is a general-purpose, multi-paradigm, compiled programming language developed by Apple Inc. for iOS, macOS, watchOS, tvOS, Linux, and z/OS. Swift is designed to work with Apple's Cocoa and Cocoa Touch frameworks and the large body of existing Objective-C code written for Apple products."
+            }
+          />
+          <Card
+            name={"React Native"}
+            title={"JavaScript framework"}
+            desc={
+              "It’s based on React, Facebook’s JavaScript library for building user interfaces, but instead of targeting the browser, it targets mobile platforms. "
+            }
+          />
+          <Card
+            name={"Node.js"}
+            title={"Javascript outside of browser"}
+            desc={
+              "Node.js is an open-source, cross-platform JavaScript run-time environment that executes JavaScript code outside of a browser."
             }
           />
         </Slider>
